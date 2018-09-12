ea3de3a84e84d2943a7ed1008d395ad9e5039104
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-09-12 14:28:36 +0200

    Update add Readme, clean up code

---
+++ README.md
@@ -0,0 +1,99 @@
+# WP Challenge > Landing page (half a day / full day)
+
+(times are given for the champs)
+
+## Alpha 1: Client wants a landing page saying when the product will be launched
+    13h30 (45m) 9h30
+    New WP install
+    New theme (no parent, starter: underscores)
+    Page with new Template => homepage
+    NB: Préciser qu'il faut le faire le plus vite possible (sinon ils le font direct dans WP)
+
+## Alpha 2: Product launch date changed
+    14h15 (10m) 10h
+    Client wants to be able to change the introduction text and the date
+    Watch out for use of the_meta() => go get *only* the key needed instead => get_post_meta()
+
+
+-- PAUSE --
+    WPDB https://codex.wordpress.org/Class_Reference/wpdb
+    > insert, prepare
+
+## Beta 1: Client wants to save visitors names and emails
+    14h25 (35m) 10h15
+    Add a form with email, name
+    NB: Check that they show a confirmation message when the email is saved
+    Requirements: https://codex.wordpress.org/Class_Reference/wpdb
+    Warning: Using 'name' as a variable in the form bugs WP
+
+## Beta 2: Client wants more options
+    15h00 (1h) 11h05
+    Save choice of HTML/Text newsletter preference
+
+
+## RC 1: Client wants to be able to change the thank you text and image
+    16h (30m) 11h15
+    Means: activating featured image (functions.php) [not anymore]
+    permissions issues uploading: chgroup chmod (use chmod g+w instead of 775)
+
+    Done: 11h40
+    Group who finished first goes around and helps others moving on
+
+-- PAUSE --
+    Admin Menu: https://developer.wordpress.org/plugins/administration-menus/
+
+## RC 2: Client want to see all details that have been saved
+    16h30 - 17h Done. (30m)
+    Means: adding a new menu entry in the backend
+    Requirements: Add admin menu
+
+## Optional:
+    - Add a countdown to the launch date
+    - Add a counter of people already registered
+    - Prepare message when launch date passed
+    - Client wants to receive an email when someones leaves his details
+
+## Question
+    Where could such a site (WP) be hosted? Free options?
+    What solutions could be used to send the newsletter?
+
+
+## Outcome
+    Two ways:
+        - deliver quickly maybe spend more time later
+        - spend more time, client waits more (at first, maybe less later, but it's a bet)
+
+
+## Bones
+
+Add contact page (without plugin)
+    => Check for SPAM / robots
+How does it print?
+Export to PDF?
+
+Pluging to save emails (subscribe)
+Page Subscribe
+    Formulaire pour envoyer son email
+    Compteur nombre de personnes inscrites
+
+
+Plugin New product launch
+
+    Admin   => Enter a date
+            (+) Text fields (before / after)
+    Page    => Shows days until date
+            => If date expired show message
+
+
+    places left = xxx
+    nombre jours left
+
+Code review
+    How to improve code
+    What functions were used, how
+
+
+
+CSS
+
+To generate the style.css run: `sass --watch scss:.`
--- functions.php
+++ functions.php
@@ -1,50 +1,90 @@
 <?php
+
+// Add Stylesheet
 function product_launch_enqueue_style(){
     wp_enqueue_style( 'product_launch', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'product_launch_enqueue_style' );
 
-function add_visitor($name, $email, $format){
-    global $wpdb;
-    if ($name != "" && $email != "") {
-        $visitor = $wpdb->get_results( $wpdb->prepare("SELECT email FROM visitors WHERE email = %s", $email), OBJECT);
-        if (!empty($visitor)) {
-            $msg = "Email already registered.";
-        } else {
-            $wpdb->insert(
-            	'visitors',
-            	array(
-            		'name' => $name,
-            		'email' => $email,
-                    'format' => $format
-            	));
-            $msg = get_post_meta(6, 'message', true);
-        }
-    } else {
-        $msg = "All fields are required.";
+// Save visitor in database (wpdb)
+function add_visitor($name, $email, $format) {
+
+    // Check required fields
+    if ($name == "" || $email == "") {
+        return "All fields are required.";
     }
 
-    return $msg;
-}
-
-add_theme_support('post-thumbnails');
-function visitors_list(){
-    add_menu_page('visitors\' list','visitors\' list', 'manage_options','visitors-list','display_visitors', 'dashicons-media-text' );
-}
-
-function display_visitors(){
+    // Check if visitor exits
     global $wpdb;
-    $list = $wpdb->get_results('SELECT name , email , format FROM visitors');
-    echo "<table> ";
-    foreach ($list as $visitor) {
-        echo "<tr>";
-        echo "<td>$visitor->name</td>";
-        echo "<td>$visitor->email</td>";
-        echo "<td>$visitor->format</td>";
-        echo "</tr>";
+    $visitor = $wpdb->get_results(
+        $wpdb->prepare("SELECT email FROM visitors WHERE email = %s", $email),
+        OBJECT
+    );
+    if (!empty($visitor)) {
+        return "Email already registered.";
     }
-    echo "</table> ";
+
+    // Insert data
+    $wpdb->insert(
+        'visitors',
+        array(
+            'name' => $name,
+            'email' => $email,
+            'format' => $format
+        )
+    );
+
+    // All went well, get the thank you message
+    $msg = get_post_meta(6, 'message', true);
 }
 
-add_action('admin_menu', 'visitors_list');
-?>
+// Enable post thumbnails
+add_theme_support('post-thumbnails'); // Not needed anymore?
+
+// Admin menu
+function gp_visitors_list() {
+    add_menu_page(
+        'Product Launch', // Page title
+        'Product Launch', // Menu entry
+        'manage_options', // Capability
+        'visitors-list', // Menu slug
+        'gp_display_visitors', // Function called
+        'dashicons-media-text' // Icon
+    );
+}
+
+// Show visitors listing
+function gp_display_visitors() {
+
+    global $wpdb;
+    $list = $wpdb->get_results('SELECT name, email, format FROM visitors');
+
+    ?>
+    <section class="wrap">
+        <h2>Product Launch data</h2>
+        <table class="wp-list-table widefat fixed striped posts">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Format</th>
+                </tr>
+            </thead>
+            <tbody><?php
+
+                foreach ($list as $visitor) {
+                    echo "<tr>";
+                    echo "<td>$visitor->name</td>";
+                    echo "<td>$visitor->email</td>";
+                    echo "<td>$visitor->format</td>";
+                    echo "</tr>";
+                }
+                
+            ?></tbody>
+        </table>
+    </section>
+    <?php
+}
+
+// Hook up new menu
+add_action('admin_menu', 'gp_visitors_list');
